38ea50691caab50564e43f2e89cc813af452f2cc
snhkn <senihaakin@gmail.com>
2023-12-12 16:30:25 +0300

    Use and set up alexedwards/scs sessions package

--- Go/HelloWorld/cmd/web/main.go
+++ Go/HelloWorld/cmd/web/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/snhkn/100DaysOfCode/Go/HelloWorld/pkg/config"
 	"github.com/snhkn/100DaysOfCode/Go/HelloWorld/pkg/handlers"
 	"github.com/snhkn/100DaysOfCode/Go/HelloWorld/pkg/render"
@@ -12,8 +14,22 @@ import (
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+
+	// change this to true in production
+	// set to false in development mode
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tmplCache, err := render.CreateTemplateCache()
 
