f432025dc50ed725251b53c070aaf35e96f1bb8e
snhkn <senihaakin@gmail.com>
2023-12-12 21:33:21 +0300

    Experiment with sessions

--- Go/HelloWorld/pkg/handlers/handlers.go
+++ Go/HelloWorld/pkg/handlers/handlers.go
@@ -29,6 +29,8 @@ func NewHandlers(r *Repository) {
 }
 
 func (m *Repository) HomeHandler(w http.ResponseWriter, r *http.Request) {
+	remoteIp := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
@@ -38,8 +40,10 @@ func (m *Repository) AboutHandler(w http.ResponseWriter, r *http.Request) {
 	strMap := make(map[string]string)
 	strMap["test"] = "Hello, again"
 
-	//send the data to the template
+	remoteIp := m.App.Session.GetString(r.Context(), "remote_ip")
+	strMap["remote_ip"] = remoteIp
 
+	//send the data to the template
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
 		StringMap: strMap,
 	})
